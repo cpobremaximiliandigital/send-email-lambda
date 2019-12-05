e1284022026be7a4598c09198554c5fbd5e2b4dc
Charles Pobre <cpobre.peg@gmail.com>
2019-12-05 12:13:59 +0800

    Remove SNS topic

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
 const aws = require('aws-sdk');
 aws.config.update({region: 'us-west-2'});
-const sns = new aws.SNS()
-
-exports.handler = async function(event, context) {
-  const params = {
-    Message: 'Hello World! Charles',
-    Subject: 'SNS Notification from Lambda',
-    TopicArn: process.env.SNS_TOPIC_ARN
-  };
-  try {
-    await sns.publish(params).promise()
-    return { statusCode: 200, body: 'Message sent' };
-  } catch(err) {
-    return { statusCode: 500, body: JSON.stringify(err) };
-  }
-};
 
 exports.sendEmail = async function(event, context) {
   console.log("Handling email to", event);
